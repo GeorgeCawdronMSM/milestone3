b63ef34d7d21db85231710e881e2a4e698f80a13
george.cawdron <george.cawdron@moneysupermarket.com>
2018-10-16 14:02:53 +0100

    daniel help level 1

--- src/main/java/task3/Application.java
+++ src/main/java/task3/Application.java
@@ -19,7 +19,7 @@ public class Application {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 //this is a comment add to my stuff
-            System.out.println(("heres a string yoooo"));
+            System.out.println(("heres a string yoooomoreee hangessssss"));
             System.out.println("Let's inspect the beans and also look to see a change to the codeeeee provided by Spring Boot:");
 
             String[] beanNames = ctx.getBeanDefinitionNames();
